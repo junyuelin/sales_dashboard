6633737dfa8246e250622c7875b3490aa63ec6d8
junyuelin <junyuelin608@gmail.com>
2024-05-07 15:56:38 -0700

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Line Chart</title>
-    <script src="https://d3js.org/d3.v7.min.js"></script>
+    <script src="https://d3js.org/d3.v6.js"></script>
     <style>
         /* CSS styling */
         body {
@@ -14,8 +14,8 @@
         }
 
         #chart {
-            width: 600px;
-            height: 400px;
+            width: 900px;
+            height: 1400px;
             border: 1px solid #ccc;
         }
 
@@ -40,96 +40,77 @@
 <body>
     <div>
         <label for="product-select">Select Product:</label>
-        <select id="product-select">
-            <!-- Dropdown options will be populated dynamically -->
-            <option value="Men's Street Footwear">Men's Street Footwear</option>
-            <option value="Men's Athletic Footwear">Men's Athletic Footwear</option>
-            <option value="Women's Street Footwear">Women's Street Footwear</option>
-            <option value="Women's Athletic Footwear">Women's Athletic Footwear</option>
-            <option value="Men's Apparel">Men's Apparel</option>
-            <option value="Women's Apparel">Women's Apparel</option>
-        </select>
+        <select id="product-select"></select>
     </div>
     <svg id="chart"></svg>
 
     <script>
-        // JavaScript code
         // Fetch data from CSV file and draw line chart
-        d3.csv("static/cleaned.csv").then(function(data) {
+        d3.csv("https://raw.githubusercontent.com/junyuelin/sales_dashboard/main/static/cleaned.csv").then(function(data) {
             // Function to update chart based on selected product
             function updateChart(selectedProduct) {
+                d3.select("#chart").selectAll("*").remove();
                 // Filter data based on selected product
-                var filteredData = data.filter(function(d) {
-                    return d.Product === selectedProduct;
-                });
+                var filteredData = data.filter(d => d.Product === selectedProduct);
 
                 // Sum up operating profit by month
-                var sumByMonth = d3.rollup(filteredData, v => d3.sum(v, d => d['Operating Profit']), d => d.Month);
+                var sumByMonth = d3.rollup(filteredData, v => d3.sum(v, d => +d['Operating Profit']), d => d.Month);
 
                 // Convert data to array for D3.js
                 var dataArray = Array.from(sumByMonth, ([key, value]) => ({ Month: key, 'Operating Profit': value }));
 
-                // Your D3.js code to draw the line chart goes here
-                // For example:
                 var svg = d3.select("#chart"),
-                    margin = {top: 20, right: 20, bottom: 30, left: 50},
+                    margin = { top: 320, right: 20, bottom: 30, left: 50 },
                     width = +svg.attr("width") - margin.left - margin.right,
                     height = +svg.attr("height") - margin.top - margin.bottom,
                     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-                var parseTime = d3.timeParse("%Y-%m-%d");
-                var formatTime = d3.timeFormat("%b %Y");
-
-                var x = d3.scaleTime()
-                    .rangeRound([0, width]);
+                var x = d3.scaleLinear()
+                          .domain([1, 12]) // Assuming months from 1 to 12
+                          .range([0, width]);
 
                 var y = d3.scaleLinear()
-                    .rangeRound([height, 0]);
+                          .domain([0, d3.max(dataArray, d => d['Operating Profit'])]) // Scale based on the max profit
+                          .range([height, 0]);
 
                 var line = d3.line()
-                    .x(function(d) { return x(parseTime(d.Month)); })
-                    .y(function(d) { return y(+d['Operating Profit']); });
-
-                x.domain(d3.extent(dataArray, function(d) { return parseTime(d.Month); }));
-                y.domain(d3.extent(dataArray, function(d) { return +d['Operating Profit']; }));
+                             .x(d => x(+d.Month))
+                             .y(d => y(d['Operating Profit']));
 
                 g.append("g")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(x))
-                    .select(".domain")
-                    .remove();
+                 .attr("transform", "translate(0," + height + ")")
+                 .call(d3.axisBottom(x));
 
                 g.append("g")
-                    .call(d3.axisLeft(y))
-                    .append("text")
-                    .attr("fill", "#000")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", "0.71em")
-                    .attr("text-anchor", "end")
-                    .text("Operating Profit");
+                 .call(d3.axisLeft(y))
+                 .append("text")
+                 .attr("fill", "#000")
+                 .attr("transform", "rotate(-90)")
+                 .attr("y", 6)
+                 .attr("dy", "0.71em")
+                 .attr("text-anchor", "end")
+                 .text("Operating Profit");
 
                 g.append("path")
-                    .datum(dataArray)
-                    .attr("class", "line")
-                    .attr("d", line);
+                 .datum(dataArray)
+                 .attr("class", "line")
+                 .attr("d", line);
             }
 
             // Populate dropdown options with unique product names
             var productNames = Array.from(new Set(data.map(d => d.Product)));
-            var select = document.getElementById('product-select');
-            productNames.forEach(function(product) {
-                var option = document.createElement('option');
-                option.text = product;
-                option.value = product;
-                select.add(option);
+            var select = d3.select('#product-select');
+            productNames.forEach(product => {
+                select.append('option')
+                      .text(product)
+                      .attr('value', product);
             });
 
             // Initial chart rendering with default selected product
             updateChart(productNames[0]);
 
             // Event listener for dropdown change
-            document.getElementById('product-select').addEventListener('change', function() {
+            select.on('change', function() {
                 updateChart(this.value);
             });
         });
